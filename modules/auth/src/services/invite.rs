203a88625ca9f9c9d4895d5aedcc370bb0250e11
Nikaidou Haruki <nharuki@pm.me>
2026-06-10 13:04:57 +0900

    Add request/response types for invite service operations

--- modules/auth/src/services/invite.rs
+++ modules/auth/src/services/invite.rs
@@ -1,5 +1,6 @@
 use crate::entities::db::invitation::InvitationEntity;
 use kanau::processor::Processor;
+use time::PrimitiveDateTime;
 use uuid::Uuid;
 use wakuwaku::sqlx::DatabaseProcessor;
 
@@ -9,27 +10,67 @@ pub struct InviteService {
 }
 
 #[derive(Debug, Clone)]
-pub struct ListUserInvites {
+pub struct ListUserInvitesRequest {
     pub user_id: Uuid,
+    pub offset: i64,
+    pub limit: i64,
 }
 
-impl Processor<ListUserInvites> for InviteService {
+impl Processor<ListUserInvitesRequest> for InviteService {
     type Output = Vec<InvitationEntity>;
     type Error = wakuwaku::Error;
-    async fn process(&self, input: ListUserInvites) -> Result<Self::Output, Self::Error> {
+    async fn process(&self, input: ListUserInvitesRequest) -> Result<Self::Output, Self::Error> {
         todo!()
     }
 }
 
 #[derive(Debug, Clone)]
-pub struct CheckInviteToken {
+pub struct CheckInviteTokenRequest {
     pub token: Uuid,
 }
 
-impl Processor<CheckInviteToken> for InviteService {
+impl Processor<CheckInviteTokenRequest> for InviteService {
     type Output = Option<InvitationEntity>;
     type Error = wakuwaku::Error;
-    async fn process(&self, input: CheckInviteToken) -> Result<Self::Output, Self::Error> {
+    async fn process(&self, input: CheckInviteTokenRequest) -> Result<Self::Output, Self::Error> {
+        todo!()
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct UserRegisterRequest {
+    pub username: String,
+    pub password_plaintext: String,
+    pub use_invite: Uuid,
+    pub register_with_login: bool,
+}
+
+#[derive(Debug, Clone)]
+pub enum UserRegisterResponse {
+    Success,
+    Login { token: String },
+    InvalidInvite,
+}
+
+impl Processor<UserRegisterRequest> for InviteService {
+    type Output = UserRegisterResponse;
+    type Error = wakuwaku::Error;
+    async fn process(&self, input: UserRegisterRequest) -> Result<Self::Output, Self::Error> {
+        todo!()
+    }
+}
+
+#[derive(Debug, Clone)]
+pub struct CreateInviteRequest {
+    pub user_id: Uuid,
+    pub expire_at: PrimitiveDateTime,
+    pub max_use_count: Option<i64>,
+}
+
+impl Processor<CreateInviteRequest> for InviteService {
+    type Output = InvitationEntity;
+    type Error = wakuwaku::Error;
+    async fn process(&self, input: CreateInviteRequest) -> Result<Self::Output, Self::Error> {
         todo!()
     }
 }
